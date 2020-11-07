d8a44f1b41aec74b900dcc5a0633e6232d2480f6
J <johnnyqbui7@gmail.com>
2020-11-07 13:54:24 -0600

    add env variables, invalidate keys

--- slices/auth.js
+++ slices/auth.js
@@ -1,59 +1,59 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 // A slice for Auth with our three reducers
 const AuthSlice = createSlice({
-  name: 'auth',
+  name: "auth",
   initialState: {
     loading: false,
     hasErrors: false,
-    authData: {},
+    authData: {}
   },
   reducers: {
     getAuthData: state => {
-      state.loading = true
+      state.loading = true;
     },
     authSuccess: (state, { payload }) => {
-      state.authData = payload
-      state.loading = false
-      state.hasErrors = false
+      state.authData = payload;
+      state.loading = false;
+      state.hasErrors = false;
     },
     authFailure: state => {
-      state.loading = false
-      state.hasErrors = true
-    },
-  },
-})
+      state.loading = false;
+      state.hasErrors = true;
+    }
+  }
+});
 
 // Three actions generated from the slice
-export const { getAuthData, authSuccess, authFailure } = AuthSlice.actions
+export const { getAuthData, authSuccess, authFailure } = AuthSlice.actions;
 
 // The reducer
-export default AuthSlice.reducer
+export default AuthSlice.reducer;
 
 // A selector
-export const AuthSelector = state => state.auth
+export const AuthSelector = state => state.auth;
 
 // Asynchronous thunk action
 export function authenticate() {
   return async dispatch => {
-    dispatch(getAuthData())
-    const key = "anhH4y0izmhyhLqKw6efPobJX4SF29ZPv4lyneIrxn1dGyBT5G";
-    const secret = "m0Gnl4RQ4Ngx5HQn2dLeiKLbLtp9VQfiPIDMXGxI";
-    
+    dispatch(getAuthData());
+    const key = process.env.NEXT_PUBLIC_PETFINDER_KEY;
+    const secret = process.env.NEXT_PUBLIC_PETFINDER_SECRET;
+
     try {
       const res = await fetch("https://api.petfinder.com/v2/oauth2/token", {
         method: "POST",
         body: `grant_type=client_credentials&client_id=${key}&client_secret=${secret}`,
         headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
       });
 
       const data = await res.json();
-      
-      dispatch(authSuccess(data))
+
+      dispatch(authSuccess(data));
     } catch (error) {
-      dispatch(authFailure())
+      dispatch(authFailure());
     }
-  }
+  };
 }
